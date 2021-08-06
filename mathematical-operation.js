0eb1472dd170cdfa3342cdbbe2060bd27fb53ea0
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-06 19:12:36 +0600

    mathematical-operation.js added

---
+++ mathematical-operation.js
@@ -0,0 +1,18 @@
+var onionPrice = 42;
+var eggPrice = 8;
+var eggQuantity = 5;
+var moneyAvailable = 100;
+var orangePrice = 5;
+
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var eggPriceMultiplication = eggPrice * eggQuantity;
+var orangeQuantity = moneyAvailable / orangePrice;
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(totalPrice);
+console.log(priceDifference);
+console.log(eggPriceMultiplication);
+console.log(orangeQuantity);
+
