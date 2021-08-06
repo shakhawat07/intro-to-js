f96f3fc9b8db69a7d8ed02241b217d44763134a5
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-07 00:37:35 +0600

    conversion.js added

---
+++ conversion.js
@@ -0,0 +1,16 @@
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+var x = '3';
+var y = 4.5;
+var sum = x + y;
+console.log(sum);
+
+var price1 = '5.3';
+var price2 = '4.5';
+var price1Parse = parseInt(price1);
+var price2Parse = parseFloat(price2);
+var total = price1Parse + price2Parse;
+console.log(total);
