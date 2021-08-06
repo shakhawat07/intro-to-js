879c11d6b0a5aedcdb139048ca324f61d9d6fff3
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-06 18:40:56 +0600

    variable-name.js updated

---
+++ variable-name.js
@@ -0,0 +1,27 @@
+var name = 'Hero Alan';
+
+// reserved keyword not allowed for naming a variable
+// var false = 15;
+// var delete = 'delete';
+
+//variable name should be in one words 
+// var nayok name = 'Tom hanks';
+
+// quote is not allowed for naming convention
+// var "name" = 'Tom';
+
+// hyphen is not allowed in naming convention 
+// var my-address = 'Canada';
+
+// We can use camel case for naming 
+var myAddress = 'Canada';
+var MyAddress = 'Canada';
+
+// we can use underscore but not not a good choice
+var my_address = 'Canada';
+
+var PIE = 3.14;
+// number in the starting is not allowed 
+// var 999price = 25;
+
+var price99 = 25;
