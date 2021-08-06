e65a5a7bdf56bd3a6bafdde2674f75dc106b9b2a
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-06 23:59:40 +0600

    math-operation.js added

---
+++ math-operation.js
@@ -0,0 +1,16 @@
+var price1 = 31;
+var price2 = 10;
+
+// price1 = price1 + 19;
+price1 += 19;
+// price2 = price2 -5;
+price2 -= 5;
+
+console.log(price1);
+console.log(price2);
+
+var age = 25;
+// age = age + 1;
+// age += 1;
+age++;
+console.log(age);
