db581656decf7af610a93ec8318f52e7119ea637
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-07 00:50:31 +0600

    variable-types.js added

---
+++ variable-types.js
@@ -0,0 +1,9 @@
+var shirtPrice = 200;
+var pantPrice = '500'
+var shoePrice;
+var today = new Date();
+
+console.log(typeof shirtPrice);
+console.log(typeof pantPrice);
+console.log(typeof shoePrice);
+console.log(typeof today);
